cbc6423ee7592942f639df17f2d3740782f62fc0
Martin Fenner <mfenner@datacite.org>
2018-04-29 16:21:33 -0400

    turn off hashie warnings

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -6,24 +6,4 @@ Elasticsearch::Persistence.client = Elasticsearch::Client.new hosts: [
     scheme: 'http'
   }]
 
-  module CoreExtensions
-    module Elasticsearch
-      module Model
-  
-        # Subclass of `Hashie::Mash` to wrap Hash-like structures
-        # (responses from Elasticsearch, search definitions, etc)
-        #
-        # The primary goal of the subclass is to disable the
-        # warning being printed by Hashie for re-defined
-        # methods, such as `sort`.
-        #
-        class HashWrapper < ::Hashie::Mash
-          disable_warnings if respond_to?(:disable_warnings)
-        end
-      end
-    end
-  end
-  
-  # backport from later version:
-  # https://github.com/elastic/elasticsearch-rails/blob/master/elasticsearch-model/lib/elasticsearch/model/hash_wrapper.rb
-  Elasticsearch::Model.include CoreExtensions::Elasticsearch::Model
+Hashie.logger = Logger.new('/dev/null')
