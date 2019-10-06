6b1e580650ddc6116166a55a505dc636205a351f
Martin Fenner <martin.fenner@datacite.org>
2019-10-06 07:57:03 +0200

    additional fields for graphql. #6

--- app/graphql/types/data_catalog_type.rb
+++ app/graphql/types/data_catalog_type.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DataCatalogType < BaseObject
+  key fields: 'id'
+
   description "A collection of datasets."
 
   field :id, ID, null: false, description: "The ID of the data catalog."
@@ -10,17 +12,18 @@ class DataCatalogType < BaseObject
   field :url, String, null: true, hash_key: "repositoryUrl", description: "URL of the data catalog."
   # field :contacts, [String], null: true, description: "Repository contact information"
   field :description, String, null: true, description: "A description of the data catalog."
-  # field :certificates, [String], null: true, description: "Repository certificates"
-  # field :subjects, [SchemeType], null: true, description: "Subjects"
+  field :certificates, [DefinedTermType], null: true, description: "Certificates of the data catalog."
+  field :subjects, [DefinedTermType], null: true, description: "Subject areas covered by the data catalog."
   # field :types, [String], null: true, description: "Repository types"
   # field :content_types, [SchemeType], null: true, description: "Content types"
   # field :provider_types, [String], null: true, description: "Provider types"
+  field :in_language, [String], null: true, description: "The language of the content of the data catalog."
   field :keywords, String, null: true, description: "Keywords or tags used to describe this data catalog. Multiple entries in a keywords list are typically delimited by commas."
   # field :data_accesses, [TextRestrictionType], null: true, description: "Data accesses"
   # field :data_uploads, [TextRestrictionType], null: true, description: "Data uploads"
   # field :pid_systems, [String], null: true, description: "PID Systems"
   # field :apis, [ApiType], null: true, description: "APIs"
-  # field :software, [String], null: true, description: "Software"
+  field :software_application, [SoftwareApplicationType], null: true, description: "Software"
 
   def id
     "https://doi.org/#{object.identifier["doi"].downcase}"
@@ -36,5 +39,28 @@ class DataCatalogType < BaseObject
 
   def keywords
     Array.wrap(object.keywords).map { |k| k["text"] }.join(", ")
+  end
+
+  def in_language
+    Array.wrap(object["repositoryLanguages"]).map { |k| k["text"] }
+  end
+
+  def certificates
+    Array.wrap(object.certificates).map { |s| { "name" => s["text"] } }
+  end
+
+  def subjects
+    Array.wrap(object.subjects).map do |s|
+      term_code, name = s["text"].split(" ", 2)
+
+      {
+        "term_code" => term_code,
+        "name" => name,
+        "in_defined_term_set" => "DFG" }
+    end
+  end
+
+  def software_application
+    Array.wrap(object.software).map { |s| { "name" => s["name"] } }
   end
 end
