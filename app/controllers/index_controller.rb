1f827f2d2e83745fd76c6cc731f8ab1b875b9b85
Martin Fenner <martin.fenner@datacite.org>
2019-10-06 08:17:39 +0200

    handle get requests to graphql api endpoint. #6

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,4 +2,9 @@ class IndexController < ApplicationController
   def index
     render plain: ENV['SITE_TITLE']
   end
+
+  def method_not_allowed
+    response.set_header('Allow', 'POST')
+    render json: { "message": "This endpoint only supports POST requests." }.to_json, status: :method_not_allowed
+  end
 end
